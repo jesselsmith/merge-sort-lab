8bcb53f8db08edaf71a3220e84bf14c31a2d113e
Jesse Smith <jesse.lee.smith@gmail.com>
2021-09-08 13:51:01 -0700

    finished solution

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+function findMinAndRemoveSorted(array){
+    return array.shift()
+}
 
+function merge(array1, array2){
+    if(array1.length == 0){
+        return array2;
+    }else if(array2.length == 0){
+        return array1;
+    }
+
+    let merged = [];
+
+    while(array1.length != 0 && array2.length != 0){
+        if(array1[0] < array2[0]){
+            merged.push(findMinAndRemoveSorted(array1));
+        }else{
+            merged.push(findMinAndRemoveSorted(array2));
+        }
+    }
+
+    return [...merged, ...array1, ...array2];
+}
+
+function mergeSort(array){
+    if(array.length <= 1){
+        return array;
+    }else{
+        let firstHalf = array.slice(0, array.length/2);
+        let secondHalf = array.slice(array.length/2);
+        return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+}
